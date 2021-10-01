2d10be6f53eadc01d9dee5ef3cdcee4e0ecd7dec
justsealan <justsealan@gmail.com>
2021-10-01 21:38:00 +0100

    Complete exercise A

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,13 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+   arrayOfPeople.forEach((person) => {
+     let h1 = document.createElement("h1");
+     let h2 = document.createElement("h2");
+     h1.textContent = person.name;
+     h2.textContent = person.job;
+     content.appendChild(h1);
+     content.appendChild(h2);
+   });
 }
 
 let people = [

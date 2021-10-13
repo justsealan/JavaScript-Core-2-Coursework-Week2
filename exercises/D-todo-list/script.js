b5eb076cb38be4c1181ba25ccaa4e8fe2c931efd
justsealan <justsealan@gmail.com>
2021-10-13 21:50:06 +0100

    Complete exercise D

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,21 @@
 function todoList(todos) {
   // Write your code here...
+  var content = document.getElementById("content");
+  var ul = document.createElement("ul");
+  content.appendChild(ul);
+  for (var i = 0; i < todos.length; i++) {
+    var li = document.createElement("li");
+    li.innerHTML = todos[i].todo;
+    console.log(todos[i].todo);
+    li.addEventListener("click", function() {
+      if (this.style.textDecoration === "line-through") {
+        this.style.textDecoration = "none";
+      } else {
+        this.style.textDecoration = "line-through";
+      }
+    });
+    ul.appendChild(li);
+  }
 }
 
 const todos = [
